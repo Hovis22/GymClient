16b655ac3370ae8eacf9246a41355d71888cdca9
Hovis22 <71640623+Hovis22@users.noreply.github.com>
2023-01-25 23:40:07 +0200

    Add New Pages and Styles

---
+++ GymClient/Controllers/ClassesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymClient.Controllers
+{
+    public class ClassesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GymClient/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymClient.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
