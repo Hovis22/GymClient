bb630b83d5b174053731bd62214663237b0ac5d2
Hovis22 <71640623+Hovis22@users.noreply.github.com>
2023-01-26 21:18:32 +0200

    Add RegisterModel Connect With Server

--- GymClient/Controllers/RegisterController.cs
+++ GymClient/Controllers/RegisterController.cs
@@ -1,12 +1,99 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+using System.Numerics;
+using System.Text.Json;
+using System.Text;
+using GymClient.Models;
+using System;
 
 namespace GymClient.Controllers
 {
+
+    
     public class RegisterController : Controller
     {
+
+        IHttpClientFactory clientFactory;
+
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            clientFactory = httpClientFactory;
+
+        }
+
+
+
         public IActionResult Index()
         {
             return View();
         }
+
+
+
+
+        //    [HttpPost]
+        //    public async Task<IActionResult> OnPostAsync([Bind("Id,Name,LastName,BirthDay,Phone,Email,Sex,Password")] RegisterModel reg)
+        //{
+        //    Console.WriteLine(123);
+        //    return Redirect("/");
+        //}
+
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> GoNew(RegisterModel reg) {
+            Console.WriteLine(reg.BirthDay);
+
+            SendRegisterModel(reg);
+
+            return Redirect("/Register");
+        }
+
+
+
+        public async void SendRegisterModel(RegisterModel registerModel)
+        {
+            string url = "register";
+
+
+            var client = clientFactory.CreateClient(
+            name: "Gym");
+
+            var registermodelJson = new StringContent(
+                JsonSerializer.Serialize(registerModel),
+                Encoding.UTF8,
+                Application.Json);
+
+            using (var httpResponseMessage =
+                await client.PostAsync(url, registermodelJson))  
+            {
+
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                 
+
+                    ViewData["Rezult"] = "Ok";
+              //      await Authenticate(st);
+
+                }
+                else ViewData["Rezult"] = $"Incorrect Data";
+              //  return Page();
+
+            }
+
+
+
+
+
+
+        }
+
+
+
+
+
+
     }
+
 }
--- GymClient/Program.cs
+++ GymClient/Program.cs
@@ -3,6 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpClient(name: "Gym",
+           options =>
+           {
+               options.BaseAddress = new Uri("http://localhost:5033/");
+               //options.DefaultRequestHeaders.Accept.Add(
+               //new MediaTypeWithQualityHeaderValue(
+               //"application/json", 1.0));
+               //options.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+           });
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
